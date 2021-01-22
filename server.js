e76ddf55076f83bb0f64534e630fb2bbf0ebe699
bisesh shakya <bisesh.shakya@gmail.com>
2021-01-22 13:49:21 +0545

    Created a build file

--- server.js
+++ server.js
@@ -321,7 +321,6 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.join(__dirname, "front/build")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "front", "build", "index.html"));
   });
